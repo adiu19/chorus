4cd4b16dd92641ea3cf4ea013224d9546774cf5f
adiu19 <aditupadhyaya@gmail.com>
2026-02-01 19:50:49 -0800

    file op

--- wal/wal.go
+++ wal/wal.go
@@ -1,8 +1,13 @@
 package wal
 
 import (
+	"bufio"
+	"encoding/json"
 	"os"
+	"os/signal"
+	"path/filepath"
 	"sync"
+	"syscall"
 )
 
 type Entry struct {
@@ -19,4 +24,44 @@ type WAL struct {
 	file    *os.File // current.log handle
 	nextSeq int64    // next sequence to assign
 	index   int64    // last applied seq (-1 = none)
+}
+
+func Open(dir string) (*WAL, error) {
+	// Create dir if not exists
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return nil, err
+	}
+
+	// Open/create current.log in append mode
+	logPath := filepath.Join(dir, "current.log")
+	file, err := os.OpenFile(logPath, os.O_APPEND|os.O_CREATE|os.O_RDWR, 0644)
+	if err != nil {
+		return nil, err
+	}
+
+	w := &WAL{
+		dir:     dir,
+		file:    file,
+		nextSeq: 1,
+		index:   -1,
+	}
+
+	// Register cleanup handler for graceful shutdown
+	go w.registerCleanupHandler()
+
+	return w, nil
+}
+
+func (w *WAL) registerCleanupHandler() {
+	sigCh := make(chan os.Signal, 1)
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+	<-sigCh
+	w.Close()
+}
+
+func (w *WAL) Close() error {
+	if w.file != nil {
+		return w.file.Close()
+	}
+	return nil
 }
